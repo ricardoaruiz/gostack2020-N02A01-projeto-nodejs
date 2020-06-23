e90ae51c9054a1a4fe8b03de3e816f1b57c04dd5
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-23 18:03:32 -0300

    Adicionando a lib date-fns para tratar agendamentos no mesmo horário

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.post('/users', (request, response) => {
-  const { name, email } = request.body;
-  const user = {
-    name,
-    email,
-  };
-  return response.json(user);
-});
-
 app.listen(3333, () => {
   console.log('Server listening on port 3333');
 });
