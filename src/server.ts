e65f06c808dd2e3efabded18973aa73b31fb62fb
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-22 21:12:44 -0300

    Configurando o debug

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,16 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello World TS Node' });
+app.post('/users', (request, response) => {
+  const { name, email } = request.body;
+  const user = {
+    name,
+    email,
+  };
+  return response.json(user);
 });
 
 app.listen(3333, () => {
