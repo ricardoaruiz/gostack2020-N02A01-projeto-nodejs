900fbc3c143019a9a9e9e324c095dd672b4a9f5e
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-24 19:48:34 -0300

    Instalando o typeorm e fazendo a configuração com a base de dados

---
+++ src/database/index.ts
@@ -0,0 +1,5 @@
+import { createConnection } from 'typeorm';
+
+// Essa função vai buscar o arquivo "ormconfig.json" na estrutura do
+// projeto para realizar a conexão com a base de dados
+createConnection();
