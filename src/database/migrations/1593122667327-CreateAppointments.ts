88af1aff79e5f3aa8bbd25896ed5a902afb4d7bc
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-25 19:28:55 -0300

    Criando a tabela appoint utilizando migrations do typeorm

---
+++ src/database/migrations/1593122667327-CreateAppointments.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1593122667327
+  implements MigrationInterface {
+  // Utilizado para realizar as ações no banco
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  // Utilizado para realizar as ações inversas as realizadas no método uo
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
