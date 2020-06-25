4d6760b34df4f0c12da8089bb5225d69f84f2106
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-25 19:52:06 -0300

    Criando um model utilizando o typeorm

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,15 +1,15 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 export default class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  // Observar que nem todos os bancos tem suporte a esse tipo de dado
+  // se não tiver utilizar somente timestamp
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
