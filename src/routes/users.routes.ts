4b490f63e4b59a0ed14092710ab90452f1d5456a
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-29 17:44:48 -0300

    Instalando o multer para upload de arquivos e criando rota para trocar o avatar do usuário

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 
+// Multer para fazer o controle do upload de imagens
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
 import UsersRepository from '../repositories/UsersRepository';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const userRoutes = Router();
+const upload = multer(uploadConfig);
 
 userRoutes.get('/', async (request, response) => {
   const usersRepository = getCustomRepository(UsersRepository);
@@ -34,5 +40,20 @@ userRoutes.post('/', async (request, response) => {
     response.status(400).send({ message: error.message });
   }
 });
+
+/**
+ * Rota para receber o avatar do usuário via upload utilizando o multer
+ * como middleware
+ * PATCH => /users/avatar => multipart-form com o campo avatar
+ */
+userRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (request, response) => {
+    console.log(request.file);
+    response.json({ ok: true });
+  },
+);
 
 export default userRoutes;
