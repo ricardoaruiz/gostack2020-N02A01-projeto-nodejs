399e3c7207f2a99ff70cf349ad39f511d37175d1
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-29 20:06:30 -0300

    Expondo rota para servir arquivos estáticos que foram feitos com upload

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,17 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+// Rota para servir de forma estatica os arquivos que estão
+// na pasta de upload
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('Server listening on port 3333');
