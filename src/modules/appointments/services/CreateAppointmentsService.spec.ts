57c248dfb4b78a6b8c33055f68fe7cf89557534c
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-07-20 20:26:04 -0300

    Configurando o coverage e finalizando os testes de agendamentp

--- src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,5 +1,13 @@
 import FakeAppointmentRepository from '@modules/appointments/repositories/fake/FakeAppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
+
+// import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentRepository';
+// const appointmentRepository1: IAppointmentsRepository = {
+//   create: jest.fn(),
+//   find: jest.fn(),
+//   findByDate: jest.fn(),
+// };
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -7,6 +15,11 @@ describe('CreateAppointment', () => {
     const createAppointmentService = new CreateAppointmentService(
       appointmentRepository,
     );
+
+    // appointmentRepository1.create = jest.fn().mockReturnValue({
+    //   id: 'abcd',
+    //   providerId: '123456',
+    // });
 
     const appointment = await createAppointmentService.execute({
       provider: '123456',
@@ -17,5 +30,28 @@ describe('CreateAppointment', () => {
     expect(appointment.providerId).toBe('123456');
   });
 
-  xit('should not be able to create two appointments on the same time', () => { });
+  it('should not be able to create two appointments on the same time', async () => {
+    const appointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      appointmentRepository,
+    );
+
+    appointmentRepository.findByDate = jest.fn().mockReturnValue({});
+
+    expect(
+      createAppointmentService.execute({
+        provider: '123456',
+        date: new Date(),
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+    try {
+      await createAppointmentService.execute({
+        provider: '123456',
+        date: new Date(),
+      });
+    } catch (error) {
+      expect(error.message).toEqual('This appointment is already booked');
+    }
+  });
 });
